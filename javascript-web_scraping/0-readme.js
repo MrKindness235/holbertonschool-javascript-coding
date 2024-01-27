d23059d42e0af8a49f7909d90d64e9c8afd56416
MrKindness235 <patriciocoitino235@gmail.com>
2024-01-27 06:38:57 -0800

    Something aint right.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const filename = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(filename, 'utf-8', (err, res) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(res)
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res);
+  }
 });
